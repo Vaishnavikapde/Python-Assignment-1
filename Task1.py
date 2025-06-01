3eb1e62153d93e0e3ea1d7953088ca23892c7cca
Vaishnavikapde <105229767+Vaishnavikapde@users.noreply.github.com>
2025-06-01 17:35:45 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+No1 = int(input("Enter First Number : "))
+No2 = int(input("Enter Second Number : "))
+
+print("Addition : ",No1+No2,
+      "\nSubtraction : ",No1-No2,
+      "\nMultiplication : ",No2*No2,
+      "\nDivision : ",No1/No2)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+str1=input("Enter First Name : ")
+str2=input("Enter Last Name : ")
+
+print(f"Hello {str1} {str2} ! Welcome to the Tutedude Python Program")
